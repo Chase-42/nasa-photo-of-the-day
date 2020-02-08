f976e7caa6ebbce8ee43b39d9b8aee69201f861c
Chase-42 <chaseofthecollins@gmail.com>
2020-02-08 16:30:43 -0700

    styling tweaks, added new font

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../App.css";
-import logo from "../NASA_logo.svg"
+import logo from "../NASA_logo.svg";
 import styled from "styled-components";
 
 const NavSection = styled.div`
@@ -8,24 +8,22 @@ const NavSection = styled.div`
   justify-content: space-evenly;
   margin: 0 auto;
   padding-top: 1%;
-  width: 750px;
-  font-size: 1.5rem;
-  text-decoration: none
-  color: #FC3D21
+  width: 800px;
+  text-decoration: none;
+  h1 {
+    font-size: 6em;
+  }
 `;
 
 export default function NavBar(props) {
-  console.log(props)
+  console.log(props);
   return (
     <div>
       <NavSection>
         <img className="App-logo" src={logo} alt="nasaLogo" />
-        <a href="#" className="navTags">Home</a>
-        <a href="#" className="navTags">About</a>
-        <a href="#" className="navTags">Sign Up</a>
+        <h1>Photo Of The Day</h1>
       </NavSection>
-      <h1>Photo Of The Day</h1>
       <h3>Today's Date: {props.date}</h3>
     </div>
-  )
+  );
 }
--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,27 +1,23 @@
 import React from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const PhotoCardDiv = styled.div`
-    background: rgba(84, 84, 84, 0.50);
-    margin: 0 auto;
-    width: 750px;
-    height: auto;
-    text-align: center;
-    box-shadow: 5px 10px #103e94;
-    border-radius: 15px;
+  background: rgba(84, 84, 84, 0.5);
+  margin: 0 auto;
+  width: 750px;
+  height: auto;
+  text-align: center;
+  box-shadow: 5px 10px #103e94;
+  border-radius: 15px;
 `;
 
-
 export default function PhotoCard(props) {
-  console.log(props)
+  console.log(props);
   return (
     <PhotoCardDiv>
-      <img alt="nasaPhoto" className="nasaPhoto"
-        src={props.url} />
+      <img alt="nasaPhoto" className="nasaPhoto" src={props.url} />
       <h1>{props.title}</h1>
       <p>{props.info}</p>
     </PhotoCardDiv>
-  )
+  );
 }
-
-
