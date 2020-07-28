9e9dc726b8c6acc553d171685a661c2c1cb7de90
Chase-42 <chaseofthecollins@gmail.com>
2020-07-28 08:16:27 -0600

    tweaked box shadow and card colour a lil:

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -12,12 +12,14 @@ export default function PhotoCard(props) {
 }
 
 const PhotoCardDiv = styled.div`
-  background: rgba(84, 84, 84, 0.5);
+  background: rgba(84, 84, 84, 0.45);
   margin: 0 5% 10% 5%;
   padding-bottom: 5%;
   height: auto;
   text-align: center;
-  box-shadow: 5px 10px #103e94;
+  box-shadow: 0px 8px 17px 2px rgba(16, 62, 148, 0.7),
+    0px 3px 14px 2px rgba(16, 62, 148, 0.5),
+    0px 5px 5px -3px rgba(16, 62, 148, 0.4);
   border-radius: 15px;
   h1 {
     font-size: 3rem;
